9762b618e557338ebfd07d74f739694315a4fc03
V S <0x01code@gmail.com>
2022-04-14 16:37:43 +0300

    Hotfix 14-04-22

--- event-service-rest/src/test/java/com/restapi/rest/EventRepositoryIntegrationTest.java
+++ event-service-rest/src/test/java/com/restapi/rest/EventRepositoryIntegrationTest.java
@@ -1,29 +1,31 @@
 package com.restapi.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.restapi.EventRepository;
 import com.restapi.dto.Event;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class EventRepositoryIntegrationTest {
 
-    @Mock
+    @MockBean
     EventRepository eventRepository;
-
-    @Mock
-    Event event;
 
     final String ONE_EVENT = "{" +
             "\"id\": -1," +
@@ -33,6 +35,8 @@ public class EventRepositoryIntegrationTest {
             "\"eventType\": \"talk\"," +
             "\"dateTime\": \"2022-04-10T17:45:45.0000\"" +
             "}";
+
+    final Event EVENT_FROM_DB = new Event(1, "", "", "", "", new Date());
 
     @Autowired
     WebApplicationContext context;
@@ -44,22 +48,37 @@ public class EventRepositoryIntegrationTest {
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .build();
+
+        when(eventRepository.save(any(Event.class)))
+                .thenReturn(EVENT_FROM_DB);
     }
 
 
-//    @Test
-//    void createEvent() throws Exception {
-//
-//        mvc.perform(post("/api/v1/events")
-//                        .contentType("application/json")
-//                        .content(ONE_EVENT)
-//                )
-//                .andExpectAll(
-//                        status().is2xxSuccessful()
-//                );
-//
-//        verify(eventRepository, times(1)).save(event);
-//    }
+    @Test
+    void createEvent() throws Exception {
+
+        MvcResult result = mvc.perform(post("/api/v1/events")
+                        .contentType("application/json")
+                        .content(ONE_EVENT)
+                )
+                .andExpectAll(
+                        status().is2xxSuccessful()
+                ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode node = objectMapper.readValue(response, ObjectNode.class);
+
+        int id = -1;
+
+        if (node.has("id"))
+            id = node.get("id").asInt();
+
+        assertEquals("", id, 1);
+
+        verify(eventRepository, times(1)).save(any(Event.class));
+    }
 
 
 
