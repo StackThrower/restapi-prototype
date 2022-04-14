90e23d57719736821708890a80b590ecbbe7663d
V S <0x01code@gmail.com>
2022-04-14 14:04:12 +0300

    Hotfix 14-04-22

--- event-service-rest/src/test/java/com/restapi/rest/EventRepositoryIntegrationTest.java
+++ event-service-rest/src/test/java/com/restapi/rest/EventRepositoryIntegrationTest.java
@@ -1,22 +1,29 @@
 package com.restapi.rest;
 
+import com.restapi.EventRepository;
+import com.restapi.dto.Event;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class MainControllerTest {
+public class EventRepositoryIntegrationTest {
 
-    @Autowired
-    WebApplicationContext context;
+    @Mock
+    EventRepository eventRepository;
+
+    @Mock
+    Event event;
 
     final String ONE_EVENT = "{" +
             "\"id\": -1," +
@@ -27,6 +34,9 @@ class MainControllerTest {
             "\"dateTime\": \"2022-04-10T17:45:45.0000\"" +
             "}";
 
+    @Autowired
+    WebApplicationContext context;
+
     private MockMvc mvc;
 
     @BeforeEach
@@ -36,24 +46,22 @@ class MainControllerTest {
                 .build();
     }
 
+
     @Test
-    void eventList200() throws Exception {
-        mvc.perform(get("/api/v1/events"))
+    void createEvent() throws Exception {
+
+        mvc.perform(post("/api/v1/events")
+                        .contentType("application/json")
+                        .content(ONE_EVENT)
+                )
                 .andExpectAll(
                         status().is2xxSuccessful()
                 );
+
+        verify(eventRepository, times(1)).save(event);
     }
 
-//    @Test
-//    void createEvent() throws Exception {
-//
-//        mvc.perform(post("/api/v1/events")
-//                        .contentType("application/json")
-//                        .content(ONE_EVENT)
-//                )
-//                .andExpectAll(
-//                        status().is2xxSuccessful()
-//                );
-//    }
+
+
 
 }
